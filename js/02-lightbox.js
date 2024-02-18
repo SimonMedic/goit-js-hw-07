91fc7789b6d5dd551ae1849d2921ee2f3b1f8507
SimonMedic <szymonpaluch@hotmail.com>
2024-02-18 16:23:56 +0100

    update

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryElement = document.querySelector(".gallery");
+const galleryList = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+  <a href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>`
+  )
+  .join("");
+
+galleryElement.insertAdjacentHTML("beforeend", galleryList);
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
